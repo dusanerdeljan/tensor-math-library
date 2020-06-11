3ea4c958ee7938869654b41850cf92453ddb2b49
dusanerdeljan <dusanerdeljan99@gmail.com>
2020-06-11 13:52:34 +0200

    TBB sum backend.

--- tml/Main.cpp
+++ tml/Main.cpp
@@ -15,15 +15,15 @@ void TestProfile()
 	tml::Matrix<int> m1(10000, 10000);
 	tml::Matrix<int> m2(10000, 10000);
 	auto t1 = std::chrono::high_resolution_clock::now();
-	auto result = tml::eager::SumColumns(m1, tml::execution::seq);
+	auto result = tml::eager::SumColumns(m1, tml::execution::omp);
 	auto t2 = std::chrono::high_resolution_clock::now();
 	std::cout << "Elapsed time: " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << "ms." << std::endl;
 	std::cout << result.GetShape() << std::endl;
 	t1 = std::chrono::high_resolution_clock::now();
-	auto r2 = tml::eager::SumColumns(m2, tml::execution::omp);
+	auto r2 = tml::eager::SumColumns(m2, tml::execution::tbb);
 	t2 = std::chrono::high_resolution_clock::now();
 	std::cout << "Elapsed time: " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << "ms." << std::endl;
-	std::cout << result.GetShape()<< std::endl;
+	std::cout << result.GetShape() << std::endl;
 	//std::cout << result << std::endl;
 }
 
@@ -34,7 +34,7 @@ int main()
 	tml::Matrix<int> m2 = tml::Matrix<int>::Arange(4, 4);
 	tml::Matrix<int> m3 = tml::Matrix<int>::Arange(4, 4);
 	std::cout << m1 << std::endl;
-	std::cout << tml::eager::SumColumns(m1, tml::execution::seq) << std::endl;
+	std::cout << tml::eager::SumColumns(m1, tml::execution::tbb) << std::endl;
 	std::cout << tml::eager::SumColumns(m1, tml::execution::omp) << std::endl;
 	//std::cout << tml::eager::Matmul(m1, m2, tml::execution::omp) << std::endl;
 	std::cin.get();
---
+++ tml/include/ops/eager/parallel/backends/tbb/Sum.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#if TML_HAS_TBB
+#include <numeric>
+#include <tbb/parallel_for.h>
+#include <tbb/parallel_reduce.h>
+#include "../../../base/Base.hpp"
+
+namespace tml
+{
+	namespace eager
+	{
+		namespace details
+		{
+			namespace backend
+			{
+				template<typename Scalar>
+				struct SumBackend<Scalar, TBB>
+				{
+					TML_STRONG_INLINE void Sum(const tml::Matrix<Scalar>& matrix, Scalar& result)
+					{
+						std::cout << "running tbb backend" << std::endl;
+						typedef typename tml::Matrix<Scalar>::const_iterator iter;
+						result = tbb::parallel_reduce(tbb::blocked_range<iter>(matrix.cbegin(), matrix.cend(), 200),
+							static_cast<Scalar>(0),
+							[&](const tbb::blocked_range<iter>& range, Scalar sum) -> Scalar
+						{
+							return std::accumulate(range.begin(), range.end(), sum);
+						}, std::plus<Scalar>());
+					}
+
+					TML_STRONG_INLINE void Rows(const tml::Matrix<Scalar>& matrix, tml::Matrix<Scalar>& result)
+					{
+						std::cout << "running tbb backend" << std::endl;
+						size_t cols = matrix.Columns();
+						tbb::parallel_for(tbb::blocked_range<size_t>(0, matrix.Rows(), 100), [&](const tbb::blocked_range<size_t>& range)
+						{
+							for (size_t i = range.begin(); i != range.end(); ++i)
+								result[i] = std::accumulate(matrix.cbegin() + i*cols, matrix.cbegin() + (i + 1)*cols, static_cast<Scalar>(0));
+						});
+					}
+
+					TML_STRONG_INLINE void Columns(const tml::Matrix<Scalar>& matrix, tml::Matrix<Scalar>& result)
+					{
+						std::cout << "running tbb backend" << std::endl;
+						size_t rows = matrix.Rows(), cols = matrix.Columns();
+						tbb::parallel_for(tbb::blocked_range<size_t>(0, rows, 100), [&](const tbb::blocked_range<size_t>& range)
+						{
+							for (size_t j = range.begin(); j != range.end(); ++j)
+							{
+								Scalar colSum = static_cast<Scalar>(0);
+								for (size_t i = 0; i < rows; ++i)
+									colSum += matrix[j + i*cols];
+								result[j] = colSum;
+							}
+						});
+					}
+				};
+			}
+		}
+	}
+}
+#endif
