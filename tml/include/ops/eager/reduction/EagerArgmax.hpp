5a916d804fa042efcb3c83d32642b0db501f106a
dusanerdeljan <dusanerdeljan99@gmail.com>
2020-06-12 16:52:24 +0200

    Basic argmax reduction backend.

--- tml/include/ops/eager/reduction/EagerArgmax.hpp
+++ tml/include/ops/eager/reduction/EagerArgmax.hpp
@@ -9,72 +9,54 @@ namespace tml
 {
 	namespace eager
 	{
-		template<typename Scalar>
-		Scalar Argmax(const tml::Matrix<Scalar>& matrix, tml::ExecutionPolicy execPolicy = tml::SERIAL)
+		template<typename Scalar, typename Backend = details::SEQ>
+		TML_INLINE Scalar Argmax(const tml::Matrix<Scalar>& matrix, Backend backend = tml::execution::seq)
 		{
 			Scalar result;
-			if (execPolicy == tml::SERIAL)
-				details::SerialArgmaxAll(matrix, result);
-			else
-				details::ParallelArgmaxAll(matrix, result);
+			details::backend::ArgmaxBackend<Scalar, Backend>::Argmax(matrix, result);
 			return result;
 		}
 
-		template<typename Scalar, typename T>
-		Scalar Argmax(const ExprOP<Scalar, T>& expr, tml::ExecutionPolicy execPolicy = tml::SERIAL)
+		template<typename Scalar, typename T, typename Backend = details::SEQ>
+		TML_INLINE Scalar Argmax(const ExprOP<Scalar, T>& expr, Backend backend = tml::execution::seq)
 		{
 			const tml::Matrix<Scalar> matrix = expr;
 			Scalar result;
-			if (execPolicy == tml::SERIAL)
-				details::SerialArgmaxAll(matrix, result);
-			else
-				details::ParallelArgmaxAll(matrix, result);
+			details::backend::ArgmaxBackend<Scalar, Backend>::Argmax(matrix, result);
 			return result;
 		}
 
-		template<typename Scalar>
-		tml::Matrix<Scalar> ArgmaxRows(const tml::Matrix<Scalar>& matrix, tml::ExecutionPolicy execPolicy = tml::SERIAL)
+		template<typename Scalar, typename Backend = details::SEQ>
+		TML_INLINE tml::Matrix<Scalar> ArgmaxRows(const tml::Matrix<Scalar>& matrix, Backend backend = tml::execution::seq)
 		{
 			tml::Matrix<Scalar> result(Shape{ 1, matrix.Rows() });
-			if (execPolicy == tml::SERIAL)
-				details::SerialArgmaxRows(matrix, result);
-			else
-				details::ParallelArgmaxRows(matrix, result);
+			details::backend::ArgmaxBackend<Scalar, Backend>::Rows(matrix, result);
 			return result;
 		}
 
-		template<typename Scalar, typename T>
-		tml::Matrix<Scalar> ArgmaxRows(const ExprOP<Scalar, T>& expr, tml::ExecutionPolicy execPolicy = tml::SERIAL)
+		template<typename Scalar, typename T, typename Backend = details::SEQ>
+		TML_INLINE tml::Matrix<Scalar> ArgmaxRows(const ExprOP<Scalar, T>& expr, Backend backend = tml::execution::seq)
 		{
 			tml::Matrix<Scalar> result(Shape{ 1, expr.shape.Rows });
 			const tml::Matrix<Scalar> matrix = expr;
-			if (execPolicy == tml::SERIAL)
-				details::SerialArgmaxRows(matrix, result);
-			else
-				details::ParallelArgmaxRows(matrix, result);
+			details::backend::ArgmaxBackend<Scalar, Backend>::Rows(matrix, result);
 			return result;
 		}
 
-		template<typename Scalar>
-		tml::Matrix<Scalar> ArgmaxColumns(const tml::Matrix<Scalar>& matrix, tml::ExecutionPolicy execPolicy = tml::SERIAL)
+		template<typename Scalar, typename Backend = details::SEQ>
+		TML_INLINE tml::Matrix<Scalar> ArgmaxColumns(const tml::Matrix<Scalar>& matrix, Backend backend = tml::execution::seq)
 		{
 			tml::Matrix<Scalar> result(Shape{ 1, matrix.Columns() });
-			if (execPolicy == tml::SERIAL)
-				details::SerialArgmaxColumns(matrix, result);
-			else
-				details::ParallelArgmaxColumns(matrix, result);
+			details::backend::ArgmaxBackend<Scalar, Backend>::Columns(matrix, result);
 			return result;
 		}
 
-		template<typename Scalar, typename T>
-		tml::Matrix<Scalar> ArgmaxColumns(const ExprOP<Scalar, T>& expr, tml::ExecutionPolicy execPolicy = tml::SERIAL)
+		template<typename Scalar, typename T, typename Backend = details::SEQ>
+		TML_INLINE tml::Matrix<Scalar> ArgmaxColumns(const ExprOP<Scalar, T>& expr, Backend backend = tml::execution::seq)
 		{
 			tml::Matrix<Scalar> result(Shape{ 1, expr.shape.Columns });
 			const tml::Matrix<Scalar> matrix = expr;
-			if (execPolicy == tml::SERIAL)
-				details::SerialArgmaxColumns(matrix, result);
-			else
-				details::ParallelArgmaxColumns(matrix, result);
+			details::backend::ArgmaxBackend<Scalar, Backend>::Columns(matrix, result);
 			return result;
 		}
 	}
