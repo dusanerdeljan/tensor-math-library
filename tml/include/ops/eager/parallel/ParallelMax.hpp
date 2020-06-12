f000a6260be449982cf0571fad210406ce3bf2e7
dusanerdeljan <dusanerdeljan99@gmail.com>
2020-06-12 17:58:07 +0200

    OpenMP max reduction backend.

--- tml/include/ops/eager/parallel/ParallelMax.hpp
+++ tml/include/ops/eager/parallel/ParallelMax.hpp
@@ -1,3 +1,4 @@
 #pragma once
 
 #include "backends/tbb/Max.hpp"
+#include "backends/omp/Max.hpp"
--- tml/include/ops/eager/parallel/backends/omp/OMPBackend.hpp
+++ tml/include/ops/eager/parallel/backends/omp/OMPBackend.hpp
@@ -3,3 +3,4 @@
 #include "UnaryOP.hpp"
 #include "BinaryOP.hpp"
 #include "Sum.hpp"
+#include "Max.hpp"
