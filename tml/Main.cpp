c2768daad50269ea6d508698ce5cb41195f09e70
dusanerdeljan <dusanerdeljan99@gmail.com>
2020-06-13 21:05:51 +0200

    Restructured repository.

--- tml/Main.cpp
+++ tml/Main.cpp
@@ -2,19 +2,12 @@
 #include <chrono>
 #include "include/tml.hpp"
 
-/*!
-* TODOS:
-*
-* 1. C++ 17 parallel stl for reduction ops
-*
-*/
-
 void TestProfile()
 {
 	tml::Matrix<int> m1(10000, 10000);
 	tml::Matrix<int> m2(10000, 10000);
 	auto t1 = std::chrono::high_resolution_clock::now();
-	int result = tml::eager::Argmax(m1, tml::execution::stl);
+	int result = tml::eager::Argmax(m1, tml::execution::tbb);
 	auto t2 = std::chrono::high_resolution_clock::now();
 	std::cout << "Elapsed time: " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << "ms." << std::endl;
 	std::cout << result << std::endl;
